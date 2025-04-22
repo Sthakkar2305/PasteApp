f79d38c7b3d83b4a771f81fdbdf5e9781551fbd8
smit thakkar <sthakkar837@gmail.com>
2025-04-22 18:28:41 +0530

    updated

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { addToPastes, updateToPastes } from "../redux/pasteSlice";
-// import { FaRegCopy } from "react-icons/fa";
+import { FaRegCopy } from "react-icons/fa";
 import toast from "react-hot-toast";
 
 const Home = () => {
@@ -83,7 +83,7 @@ const Home = () => {
             className="absolute top-3 right-3 text-gray-600 hover:text-blue-600 transition"
             title="Copy"
           >
-            {/* <FaRegCopy size={18} /> */}
+            <FaRegCopy size={18} />
           </button>
 
           {/* Textarea */}
